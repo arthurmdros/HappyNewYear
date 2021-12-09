7a8e0835ed00bb2b62f22cf2d9706e516059e307
arthurmdros <arthurmedeiros2005@gmail.com>
2021-12-09 11:29:39 -0300

    Adicionando ação do elemento Check Box para salvar a presença do usuário

--- app/src/main/java/com/example/happynewyear/ui/DetailsActivity.java
+++ app/src/main/java/com/example/happynewyear/ui/DetailsActivity.java
@@ -3,13 +3,17 @@ package com.example.happynewyear.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 
 import com.example.happynewyear.R;
+import com.example.happynewyear.constant.HappyNewYearConstants;
+import com.example.happynewyear.data.SecurityPreferences;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener{
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,21 @@ public class DetailsActivity extends AppCompatActivity {
         setTheme(R.style.Theme_HappyNewYear);
         setContentView(R.layout.activity_details);
 
+        this.mSecurityPreferences = new SecurityPreferences(this);
         this.mViewHolder.checkConfirm = findViewById(R.id.check_confirm);
+        this.mViewHolder.checkConfirm.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.check_confirm){
+
+            if(this.mViewHolder.checkConfirm.isChecked()){
+                this.mSecurityPreferences.storeData(HappyNewYearConstants.PRESENCE_KEY, HappyNewYearConstants.CONFIRM_YES);
+            }else{
+                this.mSecurityPreferences.storeData(HappyNewYearConstants.PRESENCE_KEY, HappyNewYearConstants.CONFIRM_NO);
+            }
+        }
     }
 
     private static class ViewHolder{
