4ce7e28c9d9e2b7efd6bcaa94c7283fb3950529e
arthurmdros <arthurmedeiros2005@gmail.com>
2021-12-09 11:28:54 -0300

    Criando classe das constantes

---
+++ app/src/main/java/com/example/happynewyear/constant/HappyNewYearConstants.java
@@ -0,0 +1,7 @@
+package com.example.happynewyear.constant;
+
+public class HappyNewYearConstants {
+    public static String PRESENCE_KEY = "PRESENCE_KEY";
+    public static String CONFIRM_YES = "CONFIRM_YES";
+    public static String CONFIRM_NO = "CONFIRM_NO";
+}
